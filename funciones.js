8b007f76d69e7f38187d845ac76f9fb306660ca9
diegoFernandezB <diea.fernandez@duocuc.cl>
2023-05-05 19:58:22 -0400

    123

--- funciones.js
+++ funciones.js
@@ -23,7 +23,8 @@
         s
     });*/
 
-function validarform(){
+function validarform(){console.log
+
     var nombre = document.getElementById('nombre');
     var usuario = document.getElementById('usuario');
     var mail = document.getElementById('mail');
@@ -38,18 +39,28 @@ function validarform(){
     }
     if(usuario.value === null || usuario.value ==='' ){
         //mensajeError.push('Ingresa tu usuario');
+        document.getElementById('msj_usuario').innerHTML = 'Ingresa un usuario valido';
+        document.getElementById('msj_usuario').classList = 'text-warning';
     }
     if(mail.value === null || mail.value ==='' ){
         //mensajeError.push('Ingresa tu Email');
+        document.getElementById('msj_mail').innerHTML = 'Ingresa un Mail Valido';
+        document.getElementById('msj_mail').classList = 'text-warning';
     }
     if(mail2.value === null || mail2.value ==='' ){
         //mensajeError.push('ambos campos deben coincidir');
+        document.getElementById('msj_mail2').innerHTML = 'Los campos no coinciden';
+        document.getElementById('msj_mail2').classList = 'text-warning';
     }
     if(password.value === null || password.value ==='' ){
         //mensajeError.push('Ingresa tu contraseña');
+        document.getElementById('msj_pass').innerHTML = 'Contraseña invalida';
+        document.getElementById('msj_pass').classList = 'text-warning';
     }
     if(password2.value === null || password2.value ==='' ){
         //mensajeError.push('Ingresa tu contraseña');
+        document.getElementById('msj_pass2').innerHTML = 'Las contarseñas no coinciden';
+        document.getElementById('msj_pass2').classList = 'text-warning';
     }
 
     return false;
