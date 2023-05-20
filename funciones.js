9fbb1d93a1f709d40f19a818cd604616ee24a9e4
bayronfu <bayr.fuentes@duocuc.cl>
2023-05-19 21:36:07 -0400

    sdf

--- funciones.js
+++ funciones.js
@@ -31,46 +31,61 @@ function validarform(){
     if(nombre.value === null || nombre.value === '' ){
         mensajeError.push('Ingresa tu nombre');
         error.innerHTML = mensajeError
-        // document.getElementById('msj_nombre').innerHTML = 'Ingresa tu nombre poorfavor';
-        // document.getElementById('msj_nombre').classList = 'text-warning';
+    }
+    else if(nombre.value === nombre.value  ){
+        mensajeError.push(' ');
+        error.innerHTML = mensajeError
     }
      if(usuario.value === null || usuario.value ==='' ){
         mensajeError2.push('Ingresa tu usuario');
         error2.innerHTML = mensajeError2
-        // document.getElementById('msj_usuario').innerHTML = 'Ingresa un usuario valido';
-        // document.getElementById('msj_usuario').classList = 'text-warning';
     }
+    else if(usuario.value === usuario.value){
+        mensajeError2.push(' ');
+        error2.innerHTML = mensajeError
+    }
+
     if(mail.value === null || mail.value ==='' ){
         mensajeError3.push('Ingresa tu Email');
         error3.innerHTML = mensajeError3
         // document.getElementById('msj_mail').innerHTML = 'Ingresa un Mail Valido';
         // document.getElementById('msj_mail').classList = 'text-warning';
     }
+    else if(mail.value === mail.value){
+        mensajeError3.push(' ');
+        error3.innerHTML = mensajeError3
+    }
     if(mail2.value === null || mail2.value ==='' || mail.value != mail2.value){
         mensajeError4.push('ambos campos deben coincidir');
         error4.innerHTML = mensajeError4
-        // document.getElementById('msj_mail2').innerHTML = 'Los campos no coinciden';
-        // document.getElementById('msj_mail2').classList = 'text-warning';
+    }
+    else if(mail.value === mail2.value){
+        error4.style.color = 'green';
+        mensajeError4.push('ambos campos coinciden');
+        error4.innerHTML = mensajeError4
     }
     if(password.value === null || password.value ==='' ){
         mensajeError5.push('Ingresa tu contraseña');
         error5.innerHTML = mensajeError5
-        // document.getElementById('msj_pass').innerHTML = 'Contraseña invalida';
-        // document.getElementById('msj_pass').classList = 'text-warning';
+    }
+    else if(password.value === password.value){
+        mensajeError5.push(' ');
+        error5.innerHTML = mensajeError5
     }
     if(password2.value === null || password2.value ==='' ){
         mensajeError6.push('Ingresa tu contraseña');
         error6.innerHTML = mensajeError6
-    //     document.getElementById('msj_pass2').innerHTML = 'Las contarseñas no coinciden';
-    //     document.getElementById('msj_pass2').classList = 'text-warning';
     }
-    if(password2.value != password.value ){
+    else if(password2.value != password.value ){
         mensajeError6.push('Ambas contraseñas deben coincidir');
         error6.innerHTML = mensajeError6
-    //     document.getElementById('msj_pass2').innerHTML = 'Las contarseñas no coinciden';
-    //     document.getElementById('msj_pass2').classList = 'text-warning';
     }
-
+    else if(password2.value === password.value ){
+        error6.style.color = 'green';
+        mensajeError6.push('Ambas contraseñas coinciden');
+        error6.innerHTML = mensajeError6
+    }
+    
     return false;
 }
 
